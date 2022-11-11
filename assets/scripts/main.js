060dce40bfb374b0561fc3b1d58a818ecf2190c8
Avuong <avuong23@student.cccd.edu>
2022-11-10 17:21:25 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,7 +53,7 @@ function initializeServiceWorker() {
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
       try {
-        const registration = await navigator.serviceWorker.register("/sw.js", {scope: "/fa22-cse110-lab7",});
+        const registration = await navigator.serviceWorker.register("./sw.js", {scope: "/",});
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
         console.log("Service worker has been successfully registered");
